206dd039cb19a31ca3ec4305c7c1249bb7126f60
Minh Ngoc NGUYEN <mnnguyen@PROFOND.LOCAL>
2021-12-19 08:49:43 +0100

    getAllFiles

--- app/src/main/java/com/luclak/pdf2reader/getFiles.java
+++ app/src/main/java/com/luclak/pdf2reader/getFiles.java
@@ -11,8 +11,29 @@ import android.view.View;
 import androidx.core.app.ActivityCompat;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class getFiles {
+
+    private static List<File> fileList = new ArrayList<File>();
+
+    private static void getFilesList(File[] directoryListing) {
+        if (directoryListing != null) {
+            for (File f : directoryListing) {
+//                Log.d("File ", f.getAbsolutePath());
+                if (f.isFile()) {
+//                    Log.d("File ", "is file");
+                    fileList.add(f);
+                } else {
+//                    Log.d("File ", "is folder");
+                    getFilesList(f.listFiles());
+                }
+            }
+        } else {
+            Log.d("Folder ", "not found");
+        }
+    }
     public static void getFiles(Activity activity) {
         String[] requiredPermissions = { Manifest.permission.READ_EXTERNAL_STORAGE };
         ActivityCompat.requestPermissions(activity, requiredPermissions, 0);
@@ -21,13 +42,9 @@ public class getFiles {
             File downloadFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
             File[] directoryListing = downloadFolder.listFiles();
             Log.d("Folder ", downloadFolder.getPath());
-            if (directoryListing != null) {
-                for (File f : directoryListing) {
-                    Log.d("File ", f.getName());
-//                    f.get
-                }
-            } else {
-                Log.d("Folder ", "not found");
+            getFilesList(directoryListing);
+            for (File f : fileList) {
+                Log.d("File ", f.getAbsolutePath());
             }
 //            folder = downloadFolder.getPath(); // /storage/emulated/0/Download
         }
